438d78589e80ec03d9c5bbbefe560c75cdd87466
aso1901102 <1901102@s.asojuku.ac.jp>
2020-01-08 11:41:49 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/asojuku/sainttropez/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.ac.asojuku.sainttropez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
